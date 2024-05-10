02b4baefeaeb89c515bda8083bc99254cc9a401f
Ilia Stepanov <i.u.stepanov@gmail.com>
2024-05-10 12:04:33 +0300

    task done

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,3 +1,17 @@
+class Main {
+    static void main(String[] args) {
+        def list = [1, 3, 4, 5, 1, 5, 4]
+        def result = countElements(list)
+        println(result)
+    }
+    static def countElements(list) {
+        if (list == null) return [:]
 
-class main {
+        def result = [:]
+        list.each { element ->
+            result[element] = result.getOrDefault(element, 0) + 1
+        }
+
+        return result
+    }
 }
